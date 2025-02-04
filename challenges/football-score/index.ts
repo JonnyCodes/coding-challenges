a4e1a2e98493881652e2cb760aedf589f2499c9a
Jonny Blackburn <jonny.blackburn@coregaming.co.uk>
2025-02-04 10:52:29 +0000

    Added football score challenge

---
+++ challenges/football-score/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Football Score
+ * 
+ * Football scores are recorded in an array of strings, in the format "x:y", where x is our score and y is our opponents score.
+ * 
+ * Write a function that takes an array of football scores and returns the numbet if points our team has earned.
+
+ * Points are awarded for each match as follows:
+ * if x > y: 3 points (win)
+ * if x < y: 0 points (loss)
+ * if x = y: 1 point (tie)
+ */
+
+export const footballScore = (input: string[]): number => {
+
+    return;
+}
